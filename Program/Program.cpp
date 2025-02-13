81584e7398d9b9e32ab8587eb9f5017cdf32bb3b
jiseok08 <heoji0912@gmail.com>
2025-02-13 19:42:08 +0900

    (2025.2.13) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SingleLinkedList 
+{
+private:
+    int size;
+
+    struct Node
+    {
+        T data;
+        Node* next;
+
+    };
+
+    Node* head;
+public:
+    SingleLinkedList()
+    {
+        size = 0;
+        head = nullptr;
+    }
+
+    void push_front(T data)
+    {
+        Node* newNode = new Node;
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            newNode->data = data;
+            newNode->next = head;
+            
+            head = newNode;
+        }
+        size++;
+    }
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            Node* currentNode;
+
+            newNode->data = data; 
+            currentNode = newNode->data;
+
+            head = newNode;
+        }
+        size++;
+        
+    }
+};
+
+int main()
+{
+    SingleLinkedList<int> singleLinkedList;
+
+    singleLinkedList.push_front(10);
+    singleLinkedList.push_front(20);
+
+}
