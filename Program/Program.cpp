34ddbee15c41d33ad84e4547f93a96f7f9dc7651
jiseok08 <heoji0912@gmail.com>
2025-02-14 19:44:40 +0900

    (2025.2.14) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -55,15 +55,77 @@ public:
         }
         else
         {
-            Node* currentNode;
+            Node* currentNode = head;
+
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            currentNode->next = newNode;
 
             newNode->data = data; 
-            currentNode = newNode->data;
-
-            head = newNode;
+            newNode->next = nullptr;
         }
         size++;
         
+    }
+
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+
+            head = deleteNode->next;
+           
+            delete deleteNode;
+
+            size--;
+        }
+    }
+
+    void show()
+    {
+        Node* currentNode = head;
+
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " ";
+
+            currentNode = currentNode->next;
+        }
+    }
+
+    void pop_back()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is empty" << endl; 
+        }
+        else
+        {
+            Node* deleteNode = head;
+            Node* previousNode = nullptr;
+
+            while (deleteNode->next != nullptr)
+            {
+                previousNode = deleteNode;
+
+                deleteNode = deleteNode->next;
+            }
+            delete deleteNode;
+
+            previousNode->next = nullptr;
+            
+
+            size--;
+        }
+
     }
 };
 
@@ -73,5 +135,17 @@ int main()
 
     singleLinkedList.push_front(10);
     singleLinkedList.push_front(20);
+    singleLinkedList.push_back(5);
+    singleLinkedList.push_back(10);
 
+    singleLinkedList.pop_back();
+    singleLinkedList.pop_back();
+    singleLinkedList.pop_back();
+    singleLinkedList.pop_back();
+    singleLinkedList.pop_back();
+    singleLinkedList.pop_back();
+
+    singleLinkedList.show();
+
+    return 0;
 }
