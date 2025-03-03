08245c0091c18d0e7f9e5f599f8cd149543a0364
jiseok08 <heoji0912@gmail.com>
2025-03-03 19:44:44 +0900

    (2025.3.3) 완전 이진 트리

---
+++ Class8th (String)/Source.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class String
+{
+private:
+	int size;
+
+	char* pointer;
+public:
+	String()
+	{
+		size = 0;
+		pointer = nullptr;
+	}
+
+	void operator = (const char* word)
+	{
+		size = strlen(word) + 1;
+
+		if (pointer == nullptr)
+		{
+			pointer = new char[size];
+
+			for (int i = 0; i < size; i++)
+			{
+				pointer[i] = word[i];
+			}
+		}
+		else
+		{
+			char* newpointer = new char[size];
+
+			for (int i = 0; i < size; i++)
+			{
+				newpointer[i] = word[i];
+			}
+
+			delete[] pointer;
+
+			pointer = newpointer;
+		}
+	}
+
+	const int& Size()
+	{
+		return size - 1;
+	}
+
+	const char& operator [ ] (const int& index)
+	{
+		return pointer[index];
+	}
+
+	void Append(const char* word)
+	{
+		int resize = size;
+
+		size = size + strlen(word) + 1;
+
+		char* newpointer = new char[size];
+
+		for (int i = 0; i < resize; i++)
+		{
+			newpointer[i] = pointer[i];
+		}
+
+		for (int i = 0; i < strlen(word) + 1; i++)
+		{
+			newpointer[strlen(pointer) + i] = word[i];
+		}
+
+		if (pointer != nullptr)
+		{
+			delete[] pointer;
+		}
+
+		pointer = newpointer;
+
+	}
+
+	unsigned long long Find(const char* word)
+	{
+		int length = size - strlen(word);
+
+		int i = 0;
+		int j = 0;
+
+		for (i = 0; i < length; i++)
+		{
+			for (j = 0; j < strlen(word); j++)
+			{
+				if (pointer[i + j] != word[j])
+				{
+					break;
+				}
+			}
+
+			if (j == strlen(word))
+			{
+				return i;
+			}
+		}
+
+		return 18446744073709551615;
+	}
+
+	~String()
+	{
+		if (pointer != nullptr)
+		{
+			delete[] pointer;
+		}
+	}
+};
+
+int main()
+{
+	String string;
+
+	string = "apple";
+
+	string.Find("ap");
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,140 +1,63 @@
 ﻿#include <iostream>
 
+#define SIZE 6
+
 using namespace std;
 
-class String
+template<typename KEY, typename VALUE>
+class HashTable
 {
 private:
-	int size;
+    struct Node
+    {
+        KEY key;
+        VALUE value;
 
-	char* pointer;
+        Node* next;
+    };
+
+    struct Bucket
+    {
+        int count;
+        Node* head;
+    };
+
+    Bucket bucket[SIZE];
 public:
-	String()
-	{
-		size = 0;
-		pointer = nullptr;
-	}
+    HashTable()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            bucket[i].count = 0;
+            bucket[i].head = nullptr;
+        }
+    }
+    template<typename T>
+    const int& HashFunction(T key)
+    {
+        unsigned int hashIndex = (int)key % SIZE;
 
-	void operator = (const char * word)
-	{
-		size = strlen(word) + 1;
+        return hashIndex;
+    }
 
-		if (pointer == nullptr)
-		{
-			pointer = new char[size];
+    template<>
+    const int& HashFunction(const char* key)
+    {
+        int hash = 0;
 
-			for (int i = 0; i < size; i++)
-			{
-				pointer[i] = word[i];
-			}
-		}
-		else
-		{
-			char* newpointer = new char[size];
+        for (int i = 0; i < strlen(key); i++)
+        {
+            hash += key[i];
+        }
 
-			for (int i = 0; i < size; i++)
-			{
-				newpointer[i] = word[i];
-			}
+        int hashIndex = hash % SIZE;
 
-			delete [ ] pointer;
-
-			pointer = newpointer;
-		}
-	}
-
-	const int& Size()
-	{
-		return size - 1;
-	}
-
-	const char& operator [ ] (const int& index)
-	{
-		return pointer[index];
-	}
-
-	void Append(const char* word)
-	{
-		int resize = size;
-
-		size = size + strlen(word ) + 1;
-
-		char* newpointer = new char[size];
-
-		for (int i = 0; i < resize; i++)
-		{
-			newpointer[i] = pointer[i];
-		}
-		
-		for (int i = 0; i < strlen(word) + 1; i++)
-		{
-			newpointer[strlen(pointer) + i] = word[i];
-		}
-
-		if (pointer != nullptr)
-		{
-			delete[] pointer;
-		}
-
-		pointer = newpointer;
-		
-	}
-
-	long long Find(const char* word)
-	{
-		int count = 0;
-
-		for (int i = 0; i < size; i++)
-		{
-			
-		}
-	}
-
-	~String()
-	{
-		if (pointer != nullptr)
-		{
-			delete[] pointer;
-		}
-	}
+        return hashIndex;
+    }
 };
 
 int main()
 {
-	// String string;
-	// 
-	// string = "Key";
-	// 
-	// for (int i = 0; i < string.Size(); i++)
-	// {
-	// 	cout << string[i];
-	// }
-	// 
-	// cout << endl;
-	// 
-	// string = "apple";
-	// 
-	// for (int i = 0; i < string.Size(); i++)
-	// {
-	// 	cout << string[i];
-	// }
-	// 
-	// cout << endl;
-	// 
-	// string.Append(" Four");
-	// string.Append(" x");
-	// 
-	// for (int i = 0; i < string.Size(); i++)
-	// {
-	// 	cout << string[i];
-	// }
-	// cout << endl;
 
-	std::string name;
-
-	name = "AlistarAl";
-
-	cout << name.find("Al");
-
-	return 0;
+    return 0;
 }
