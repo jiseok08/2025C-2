f764eaf32926b8807ded689d7bbf04a924237ba4
jiseok08 <heoji0912@gmail.com>
2025-03-06 19:46:06 +0900

    (2025.3.6) 레드 블렉 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,11 +1,10 @@
 ﻿#include <iostream>
-#include <queue>
 
 #define SIZE 8
 
 using namespace std;
 
-template<typename T>
+template <typename T>
 class PriorityQueue
 {
 private:
@@ -22,21 +21,100 @@ public:
         }
     }
 
+    void push(T data)
+    {
+        if (index + 1 >= SIZE)
+        {
+            cout << "Priority Queue Overflow" << endl;
+        }
+        else
+        {
+            container[++index] = data;
 
+            int child = index;
+            int parent = child / 2;
+
+            while (child > 1)
+            {
+                if (container[parent] < container[child])
+                {
+                    std::swap(container[parent], container[child]);
+                }
+
+                child = parent;
+                parent = child / 2;
+            }
+        }
+    }
+
+    void pop()
+    {
+        if (index <= 0)
+        {
+            cout << "Priority Queue is Empty" << endl;
+        }
+        else
+        {
+            container[1] = container[index];
+
+            container[index--] = NULL;
+
+            int parent = 1;
+
+            while (parent * 2 <= index)
+            {
+                int child = parent * 2;
+
+                if (container[child] < container[child + 1])
+                {
+                    child++;
+                }
+
+                if (container[child] < container[parent])
+                {
+                    break;
+                }
+                else
+                {
+                    std::swap(container[parent], container[child]);
+
+                    parent = child;
+                }
+            }
+        }
+    }
+
+    const T& top()
+    {
+        return container[1];
+    }
+
+    const bool& empty()
+    {
+        if (index <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
 
 int main()
 {
-    priority_queue<int> priorityQueue;
+    PriorityQueue<int> priorityQueue;
 
-    priorityQueue.push(30);
-    priorityQueue.push(2);
-    priorityQueue.push(77);
-    priorityQueue.push(15);
+    priorityQueue.push(5);
+    priorityQueue.push(17);
+    priorityQueue.push(3);
+    priorityQueue.push(1);
+    priorityQueue.push(9);
 
     while (priorityQueue.empty() == false)
     {
-        cout << priorityQueue.top() << " ";
+        cout << priorityQueue.top() << endl;
 
         priorityQueue.pop();
     }
