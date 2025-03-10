0fe08227afe25fbd6447a70b98a5fce16acddff2
jiseok08 <heoji0912@gmail.com>
2025-03-10 15:39:14 +0900

    .

---
+++ Class10th (Priority Queue)/Class10th (Priority Queue).cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+
+#define SIZE 8
+
+using namespace std;
+
+template <typename T>
+class PriorityQueue
+{
+private:
+    int index;
+    T container[SIZE];
+public:
+    PriorityQueue()
+    {
+        index = 0;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        if (index + 1 >= SIZE)
+        {
+            cout << "Priority Queue Overflow" << endl;
+        }
+        else
+        {
+            container[++index] = data;
+
+            int child = index;
+            int parent = child / 2;
+
+            while (child > 1)
+            {
+                if (container[parent] < container[child])
+                {
+                    std::swap(container[parent], container[child]);
+                }
+
+                child = parent;
+                parent = child / 2;
+            }
+        }
+    }
+
+    void pop()
+    {
+        if (index <= 0)
+        {
+            cout << "Priority Queue is Empty" << endl;
+        }
+        else
+        {
+            container[1] = container[index];
+
+            container[index--] = NULL;
+
+            int parent = 1;
+
+            while (parent * 2 <= index)
+            {
+                int child = parent * 2;
+
+                if (container[child] < container[child + 1])
+                {
+                    child++;
+                }
+
+                if (container[child] < container[parent])
+                {
+                    break;
+                }
+                else
+                {
+                    std::swap(container[parent], container[child]);
+
+                    parent = child;
+                }
+            }
+        }
+    }
+
+    const T& top()
+    {
+        return container[1];
+    }
+
+    const bool& empty()
+    {
+        if (index <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    PriorityQueue<int> priorityQueue;
+
+    priorityQueue.push(5);
+    priorityQueue.push(17);
+    priorityQueue.push(3);
+    priorityQueue.push(1);
+    priorityQueue.push(9);
+
+    while (priorityQueue.empty() == false)
+    {
+        cout << priorityQueue.top() << endl;
+
+        priorityQueue.pop();
+    }
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,123 +1,42 @@
 ﻿#include <iostream>
 
-#define SIZE 8
+#define SIZE 10
 
 using namespace std;
 
 template <typename T>
-class PriorityQueue
+class AdjacencyMatrix
 {
 private:
-    int index;
-    T container[SIZE];
+    int size; // 정점의 개수
+    T vertex[SIZE]; // 정점의 집합
+    int matrix[SIZE][SIZE]; // 인접 행렬
 public:
-    PriorityQueue()
+    AdjacencyMatrix()
     {
-        index = 0;
+        size = 0;
 
         for (int i = 0; i < SIZE; i++)
         {
-            container[i] = NULL;
+            vertex[i] = NULL;
+
+            for (int i = 0; i < SIZE; i++)
+            {
+                matrix[i][j] = 0;
+            }
         }
     }
-
     void push(T data)
     {
-        if (index + 1 >= SIZE)
-        {
-            cout << "Priority Queue Overflow" << endl;
-        }
-        else
-        {
-            container[++index] = data;
 
-            int child = index;
-            int parent = child / 2;
-
-            while (child > 1)
-            {
-                if (container[parent] < container[child])
-                {
-                    std::swap(container[parent], container[child]);
-                }
-
-                child = parent;
-                parent = child / 2;
-            }
-        }
     }
 
-    void pop()
-    {
-        if (index <= 0)
-        {
-            cout << "Priority Queue is Empty" << endl;
-        }
-        else
-        {
-            container[1] = container[index];
-
-            container[index--] = NULL;
-
-            int parent = 1;
-
-            while (parent * 2 <= index)
-            {
-                int child = parent * 2;
-
-                if (container[child] < container[child + 1])
-                {
-                    child++;
-                }
-
-                if (container[child] < container[parent])
-                {
-                    break;
-                }
-                else
-                {
-                    std::swap(container[parent], container[child]);
-
-                    parent = child;
-                }
-            }
-        }
-    }
-
-    const T& top()
-    {
-        return container[1];
-    }
-
-    const bool& empty()
-    {
-        if (index <= 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+    void edge(int i, int j)
+ 
 };
 
 int main()
 {
-    PriorityQueue<int> priorityQueue;
-
-    priorityQueue.push(5);
-    priorityQueue.push(17);
-    priorityQueue.push(3);
-    priorityQueue.push(1);
-    priorityQueue.push(9);
-
-    while (priorityQueue.empty() == false)
-    {
-        cout << priorityQueue.top() << endl;
-
-        priorityQueue.pop();
-    }
-
+    
     return 0;
 }
